dee9d597f413121d00d66ec900bd9e68be58cfaa
sensenye <sensen.yechen@gmail.com>
2021-03-29 15:13:23 +0200

    Create meeting type & add employees

--- local-addons/odoo_meetings/__manifest__.py
+++ local-addons/odoo_meetings/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','calendar'],
+    'depends': ['base','calendar','hr'],
 
     # will appear on App menu
     'application': True,
--- local-addons/odoo_meetings/models/meeting_type.py
+++ local-addons/odoo_meetings/models/meeting_type.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class MeetingType(models.Model):
@@ -12,18 +13,27 @@ class MeetingType(models.Model):
     #     ('google_meet', 'Google Meet'),
     # ]
 
-    name = fields.Char(string="Tipo de reunión", required=True)
+    name = fields.Char(string="Tipo de reunión *", required=True)
     localization = fields.Selection([
         ('physical', 'Físico'),
         ('google_meet', 'Google Meet'),
-    ], default='google_meet', required=True)
+    ], default='google_meet', required=True, string="Localización *")
     # localization = fields.Char(string="Localización", required=True)
-    description = fields.Char(string="Descripción", required=True)
-    # employees =
-    date_range = fields.Char(string="Rango", required=True)
-    duration = fields.Integer(string="Duración", required=True)
-    daily_limit = fields.Integer(string="Límite diario", required=True)
-    url = fields.Char(string="URL", required=True)
+    description = fields.Char(string="Descripción *", required=True)
+    employees = fields.Many2many('hr.employee.public', string="Empleado")
+    # date_range = fields.Char(string="Rango", required=True)
+    # date_range = fields.Selection([
+    #     ('next_days', 'Dentro de los próximos'),
+    #     ('range', 'Dentro de un rango de fechas'),
+    #     ('infinite', 'De manera indefinida'),
+    # ], default='google_meet', required=True)
+    duration = fields.Integer(string="Duración *", required=True)
+    daily_limit = fields.Integer(string="Límite diario *", required=True)
+    url = fields.Char()
+
+    start_date = fields.Date(string='Fecha de inicio *', default=fields.Date.today())
+    end_date = fields.Date(string='Fecha de fin *', default=fields.Date.today())
+
     # localization_options = fields.Selection([
     #     ('normal', 'In Progress'),
     #     ('done', 'Ready for next stage'),
@@ -53,3 +63,52 @@ class MeetingType(models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+    # @api.constrains('field_name')
+    #  def field_name_constratints(self):
+        # if condition:
+        # raise Warning(message)
+
+    # @api.constrains('start_date','end_date')
+    # def _check_end_date(self):
+        # for record in self:
+        # if self.start_date > self.end_date:
+            # print('asdfasdfasdf')
+            # raise ValidationError("La fecha de fin tiene que ser mayor que la fecha de inicio")
+
+    @api.onchange('end_date','start_date')
+    def _onchange_date(self):
+        if self.start_date > self.end_date:
+            self.start_date = fields.Date.today()
+            self.end_date = fields.Date.today()
+            return {
+            'warning': {'title': "Warning", 'message': "La fecha de inicio tiene que ser menor que la fecha de fin", 'type': 'notification'},
+            }
+
+    # @api.onchange('start_date', 'end_date')
+    # def cheking_field_date(self):
+    #     if self.start_date and self.end_date:
+    #         if self.start_date > self.end_date:
+    #             print('asdfasdfasdf')
+    #             print(self.end_date)
+    #             self.end_date = fields.Date.today()
+    #             print(self.end_date)
+    #             raise ValidationError('The Deadline Date is Invalid')
+
+
+    # @api.onchange('end_date', 'start_date')
+    # def date_constrains(self):
+    #     for rec in self:
+    #         if rec.end_date < rec.start_date:
+    #             rec.end_date = fields.Date.today()
+    #             raise ValidationError(  'Sorry, End Date Must be greater Than Start Date...')
+
+    # pname = fields.Char(compute='_compute_pname')
+
+    # @api.depends('partner_id.start_date', 'partner_id.end_date')
+    # def _compute_pname(self):
+    #     for record in self:
+    #         if record.partner_id.start_date > record.partner_id.start_date:
+    #             record.pname = (record.partner_id.name or "").upper()
+    #         else:
+    #             record.pname = record.partner_id.name
