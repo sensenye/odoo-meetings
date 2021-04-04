b5f041a4b022b178c1883c3a098abadf9d42e399
sensenye <sensen.yechen@gmail.com>
2021-04-04 17:09:37 +0200

    Redirect to select time&date template

--- local-addons/odoo_meetings/controllers/controllers.py
+++ local-addons/odoo_meetings/controllers/controllers.py
@@ -19,22 +19,22 @@ class OdooMeetings(http.Controller):
             'meetingTypes': MeetingTypes.search([]),
         })
 
-    @http.route('/odoo-meetings/submit/', auth='public', website=True)
-    def form_submit(self, **kw):
-        print('Hola')
-        print(kw.get('meetingTypeSelect'))
-        # employee_ids = [20]
-        meeting = http.request.env['odoo_meetings.meeting_event'].create({
-            'assistant_name': kw.get('name'),
-            'assistant_email': kw.get('email'),
-            'comments': kw.get('comments'),
-            'date': kw.get('date'),
-            'hour': kw.get('hour'),
-            # 'state': 'TODO: add state',
-            # 'meeting_type': 'TODO: add meeting type',
-            # 'employee': [(6, 0, employee_ids)]
-        })
-        return http.request.render('odoo_meetings.form_success', {})
+    @http.route('/odoo-meetings/<model("odoo_meetings.meeting_type"):obj>/', auth='public', website=True)
+    def form_submittt(self, **kw):
+        # print(obj.get('name'))
+        # print(kw.get('meetingTypeSelect'))
+        # # employee_ids = [20]
+        # meeting = http.request.env['odoo_meetings.meeting_event'].create({
+        #     'assistant_name': kw.get('name'),
+        #     'assistant_email': kw.get('email'),
+        #     'comments': kw.get('comments'),
+        #     'date': kw.get('date'),
+        #     'hour': kw.get('hour'),
+        #     # 'state': 'TODO: add state',
+        #     # 'meeting_type': 'TODO: add meeting type',
+        #     # 'employee': [(6, 0, employee_ids)]
+        # })
+        return http.request.render('odoo_meetings.select_time', {})
 
 
     @http.route('/odoo-meetings/meeting-type/submit/', auth='public', website=True)
