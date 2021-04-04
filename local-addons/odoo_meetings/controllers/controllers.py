53501ef96c092e3deaee5b003a43c01435fec1fc
sensenye <sensen.yechen@gmail.com>
2021-04-04 15:33:31 +0200

    Website create meeting with dynamic description

--- local-addons/odoo_meetings/controllers/controllers.py
+++ local-addons/odoo_meetings/controllers/controllers.py
@@ -1,21 +1,73 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+
+# https://odoo-development.readthedocs.io/en/latest/dev/frontend/webpage.html   
+# https://kanakinfosystems.com/blog/create-form-in-odoo-website
+
+class OdooMeetings(http.Controller):
+    # @http.route('/odoo-meetings/<model("odoo_meetings.meeting_type"):meeting_types>', auth='public', website=True)
+    # def index(self, **kw):
+    #     # MeetingTypes = http.request.env['odoo_meetings.meeting_type']
+    #     return http.request.render('odoo_meetings.index', {
+    #         'meetingTypes': meeting_types,
+    #     })
+
+    @http.route('/odoo-meetings/', auth='public', website=True)
+    def index(self, **kw):
+        MeetingTypes = http.request.env['odoo_meetings.meeting_type']
+        return http.request.render('odoo_meetings.index', {
+            'meetingTypes': MeetingTypes.search([]),
+        })
+
+    @http.route('/odoo-meetings/submit/', auth='public', website=True)
+    def form_submit(self, **kw):
+        print('Hola')
+        print(kw.get('meetingTypeSelect'))
+        # employee_ids = [20]
+        meeting = http.request.env['odoo_meetings.meeting_event'].create({
+            'assistant_name': kw.get('name'),
+            'assistant_email': kw.get('email'),
+            'comments': kw.get('comments'),
+            'date': kw.get('date'),
+            'hour': kw.get('hour'),
+            # 'state': 'TODO: add state',
+            # 'meeting_type': 'TODO: add meeting type',
+            # 'employee': [(6, 0, employee_ids)]
+        })
+        return http.request.render('odoo_meetings.form_success', {})
 
 
-# class OdooMeetings(http.Controller):
-#     @http.route('/odoo_meetings/odoo_meetings/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+    @http.route('/odoo-meetings/meeting-type/submit/', auth='public', website=True)
+    def form_submit(self, **kw):
+        print('Hola')
+        print(kw.get('meetingTypeSelect'))
+        # employee_ids = [20]
+        meeting = http.request.env['odoo_meetings.meeting_event'].create({
+            'assistant_name': kw.get('name'),
+            'assistant_email': kw.get('email'),
+            'comments': kw.get('comments'),
+            'date': kw.get('date'),
+            'hour': kw.get('hour'),
+            # 'state': 'TODO: add state',
+            # 'meeting_type': 'TODO: add meeting type',
+            # 'employee': [(6, 0, employee_ids)]
+        })
+        return http.request.render('odoo_meetings.form_success', {})
 
-#     @http.route('/odoo_meetings/odoo_meetings/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('odoo_meetings.listing', {
-#             'root': '/odoo_meetings/odoo_meetings',
-#             'objects': http.request.env['odoo_meetings.odoo_meetings'].search([]),
-#         })
 
-#     @http.route('/odoo_meetings/odoo_meetings/objects/<model("odoo_meetings.odoo_meetings"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('odoo_meetings.object', {
-#             'object': obj
-#         })
+        # @http.route('/odoo-meetings/', authhttp.request.render('newpage.index')
+    # def index(self, **kw):
+        # return "Hello, world"
+
+    # @http.route('/odoo_meetings/odoo_meetings/objects/', auth='public')
+    # def list(self, **kw):
+    #     return http.request.render('odoo_meetings.listing', {
+    #         'root': '/odoo_meetings/odoo_meetings',
+    #         'objects': http.request.env['odoo_meetings.odoo_meetings'].search([]),
+    #     })
+
+    # @http.route('/odoo_meetings/odoo_meetings/objects/<model("odoo_meetings.odoo_meetings"):obj>/', auth='public')
+    # def object(self, obj, **kw):
+    #     return http.request.render('odoo_meetings.object', {
+    #         'object': obj
+    #     })
---
+++ local-addons/odoo_meetings/static/src/js/show_description.js
@@ -0,0 +1,50 @@
+// odoo.define("show_description", function (require) {
+//   var core = require("web.core");
+//   var rpc = require("web.rpc");
+//   // alert(core._t('Hello world'));
+//   // alert('Holii');
+
+//   // var description = rpc._rpc({
+//   //   model: 'odoo_meeting.meeting_type',
+//   //   method: 'get_description',
+//   //   // args: [some, args],
+//   // })
+
+//   // console.log('Hola');
+//   // console.log(description);
+
+//   // function listQ() {
+//   //   console.log('Hola');
+//   //   var e = document.getElementById("meetingTypeSelect");
+//   //   console.log(e.selectedIndex);
+//   //   if (e.selectedIndex == 1) {
+//       // if ("Blank Test" === e.options[e.selectedIndex].value) {
+//         alert("yo");
+//       // }
+//   //   }
+//   // }
+//   // document.getElementById("meetingTypeSelect").addEventListener("click", listQ);
+
+//   return {
+//     // if you created functionality to export, add it here
+//   };
+// });
+
+function show_description(elem) {
+  var { value, selectedIndex } = elem;
+
+  // console.log(value);
+  // console.log(selectedIndex);
+
+  var container = document.querySelector("#meetingTypeDescription");
+  var descripArray = container.querySelectorAll("p");
+
+  descripArray.forEach((desc, index) => {
+    if (desc.classList.contains("d-block"))
+      desc.classList.replace("d-block", "d-none");
+    if (selectedIndex === index) {
+      if (!desc.classList.contains("d-block"))
+        desc.classList.replace("d-none", "d-block");
+    }
+  });
+}
