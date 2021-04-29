41bdb986696574bf883ede77b581174df74656b7
sensenye <sensen.yechen@gmail.com>
2021-04-29 16:26:29 +0200

    Add google calendar dependency

--- local-addons/odoo_meetings/__manifest__.py
+++ local-addons/odoo_meetings/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','website','calendar','hr'],
+    'depends': ['base','website','calendar','hr','google_calendar'],
 
     # will appear on App menu
     'application': True,
